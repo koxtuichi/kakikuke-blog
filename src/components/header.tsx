013899bfd615958bb6769fe1379d343564dbd13a
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-02-10 02:08:04 +0900

    不要な箇所を削除

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,12 +5,11 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
+  { label: 'Profile', page: '/' },
   { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
+//TODO:画像は差し替える
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
 
 export default ({ titlePre = '' }) => {
@@ -19,7 +18,7 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} novel warehouse</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"

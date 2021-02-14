3991f56c55e978851918351febba796a4ccd1c97
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-02-15 00:33:30 +0900

    タグ一覧を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,42 @@
-import Link from 'next/link'
+import React from 'react'
 import Header from '../components/header'
-import ExtLink from '../components/ext-link'
-import Features from '../components/features'
-import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
+import getBlogIndex from '../lib/notion/getBlogIndex'
+import { getTagLink } from '../lib/blog-helpers'
+import Link from 'next/link'
 
-export default () => (
-  <>
-    <Header titlePre="Home" />
-    <div className={sharedStyles.layout}>
-      <h1>シナリオも絵も音楽もプログラムも</h1>
-      <h2 className={sharedStyles.subTitle}>-Blog Tag-</h2>
-      <div style={{ textAlign: 'center' }}>
-        <Link href={'/blog'}>
-          <a>#blog</a>
-        </Link>
+export async function getStaticProps() {
+  const postsTable = await getBlogIndex()
+  const tags = Object.keys(postsTable)
+    .filter(post => postsTable[post].Published === 'Yes')
+    .map(post => postsTable[post].Tag)
+    .filter((tag, index, self) => self.indexOf(tag) === index)
+  return {
+    props: { tags },
+  }
+}
+
+const RenderTagList = ({ tags }) => {
+  return (
+    <React.Fragment>
+      <Header titlePre="Home" />
+      <div className={sharedStyles.layout}>
+        <h1>シナリオと絵と音楽とプログラムと</h1>
+        <div style={{ marginLeft: '10px' }}>
+          <h2 className={sharedStyles.subTitle}>-Blog Tag-</h2>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {tags.map(tag => (
+            <div style={{ marginLeft: '10px', fontSize: '16px' }}>
+              <Link href={'/tag/[tag]'} as={getTagLink(tag)}>
+                <a>{tag}</a>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  </>
-)
+    </React.Fragment>
+  )
+}
+
+export default RenderTagList

6a721e2e4326f0ae0bdb1caf2463ccaf8c344b58
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-02-19 01:23:49 +0900

    プロフィール追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,28 @@ const RenderTagList = ({ tags }) => {
     <React.Fragment>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <h1>じぶんがたり。</h1>
+        <h1>みかんはあまりすきじゃない</h1>
+
+        <h2 style={{ marginTop: '65px' }}>コンセプト</h2>
+        <div style={{ textAlign: 'center' }}>
+          <p>「ゲームを日記とか小説で面白くしたい」</p>
+          <p>「自分の今日やったことを記録する場所が欲しい」</p>
+          <p>「ゲーム制作の過程を誰かに見てほしい」</p>
+          <p>「プログラミングでつまづいた所の原因と解説をしたい」</p>
+          <p>いろんなことがやりたくてそれに意味を見出したかった</p>
+          <p>だからブログっていう一つの箱に収めたかった</p>
+        </div>
+
+        <h2 style={{ marginTop: '65px' }}>仕事</h2>
+        <div style={{ textAlign: 'center' }}>
+          <p>プログラマーというかエンジニアというか</p>
+          <p>主にC#とjavascriptを使ってて</p>
+          <p>今はReact／Reduxの環境で発狂してる</p>
+          <p>苦手なのはどっちかっていうとサーバー周り</p>
+          <p>得意というか最近面白く感じてきのはcss</p>
+          <p>結果、僕はフロントエンドのプログラマーです</p>
+        </div>
+
         <div style={{ marginLeft: '10px' }}>
           <h2 className={sharedStyles.subTitle}>-Blog Tag-</h2>
         </div>

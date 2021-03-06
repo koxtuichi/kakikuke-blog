68ad406333787fa488cb5e4306590b4b8350d634
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-03-07 02:33:19 +0900

    ツイッターカード画像　　更新

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import ExtLink from './ext-link'
 import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
-import { title } from '../pages/blog/[slug]'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Profile', page: '/' },
@@ -12,7 +11,7 @@ const navItems: { label: string; page?: string; link?: string }[] = [
 ]
 
 const ogImageUrl =
-  'https://lh3.googleusercontent.com/xBwUlh5zYzZOPf4Nom73QQvuc-ON6XPNjLVTfN1GBmRRSRj-SPgSMUPLAjcNzsZYGeXLz7UFp6IxwPDl1d4JAxQQnvtFwTkJrWmrZ7x9BWVgSxFKO5YEtmc1ZULW0a6FEsHrRVKELA=w2400'
+  'https://lh3.googleusercontent.com/_-KDpzLdi8SChkJAjawZnlYIZMOJgJbwI0mDfWplbAhUZ9M5kKlvFgoBcoieXE98UjcAkZx8iX8yDAGTMeT6pJ2REWWX0_0nDDBfu0wxbnW8NuIM_Oh42lfT16IZO5WJy4hh033ENA=w2400'
 
 export default ({ titlePre = '', className = '' }) => {
   const { pathname } = useRouter()
@@ -20,13 +19,13 @@ export default ({ titlePre = '', className = '' }) => {
   return (
     <header className={`${styles.header} ${className}`}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''}</title>
+        <title>{titlePre ? `${titlePre}` : ''}</title>
         <script async src="https://cdn.splitbee.io/sb.js"></script>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
         />
-        <meta name="og:title" content={title || 'かきくけこういち.COM'} />
+        <meta name="og:title" content={titlePre || 'かきくけこういち.COM'} />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@kakikukekoichi" />
         <meta name="twitter:card" content="summary" />

cccc068296ef0b5ca8d5a3727317b2927391e9a3
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-03-10 15:15:08 +0900

    POSTをGETに修正

--- src/lib/notion/rpc.ts
+++ src/lib/notion/rpc.ts
@@ -6,18 +6,22 @@ export default async function rpc(fnName: string, body: any) {
   if (!NOTION_TOKEN) {
     throw new Error('NOTION_TOKEN is not set in env')
   }
-  const res = await fetch(`${API_ENDPOINT}/${fnName}`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      cookie: `token_v2=${NOTION_TOKEN}`,
-    },
-    body: JSON.stringify(body),
-  })
-  if (res.ok) {
-    return res.json()
-  } else {
-    throw new Error(await getError(res))
+  try {
+    const res = await fetch(`${API_ENDPOINT}/${fnName}`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        cookie: `token_v2=${NOTION_TOKEN}`,
+      },
+      body: JSON.stringify(body),
+    })
+    if (res.ok) {
+      return res.json()
+    } else {
+      throw new Error(await getError(res))
+    }
+  } catch (e) {
+    console.log(e)
   }
 }
 

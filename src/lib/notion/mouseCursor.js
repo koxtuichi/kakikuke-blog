86d9836e6083b40f95c8d4624085bdd74b92457b
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-03-12 21:09:39 +0900

    favorite　画像のホバーでカーソルカスタマイズ

--- src/lib/notion/mouseCursor.js
+++ src/lib/notion/mouseCursor.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { TweenMax } from 'gsap'
 import sharedStyles from '../../styles/shared.module.css'
+import { sleep } from './utils'
 
 export default class MouseCorsor extends React.Component {
-  componentDidMount() {
+  async componentDidMount() {
     let cursorR = 4 //カーソルの半径
     const cursor = document.getElementById('cursor') //カーソル用のdivを取得
 
@@ -12,20 +13,8 @@ export default class MouseCorsor extends React.Component {
       cursor.style.transform =
         'translate(' + e.clientX + 'px, ' + e.clientY + 'px)'
     })
-
-    //リンクにホバー時はクラスをつける
     const linkElem = document.querySelectorAll('a')
-    const imgElem = document.querySelectorAll('.hov_img')
     const imgNoLinkElem = document.querySelectorAll('.hov_img_noLink')
-
-    imgElem.forEach(elem => {
-      elem.addEventListener('mouseover', function(e) {
-        cursor.classList.add('hov_')
-      })
-      elem.addEventListener('mouseout', function(e) {
-        cursor.classList.remove('hov_')
-      })
-    })
     linkElem.forEach(elem => {
       elem.addEventListener('mouseover', function(e) {
         cursor.classList.add('hov_')
@@ -40,6 +29,17 @@ export default class MouseCorsor extends React.Component {
       })
       elem.addEventListener('mouseout', function(e) {
         cursor.classList.remove('hov_noLink')
+      })
+    })
+
+    await sleep(5000)
+    const imgElem = document.querySelectorAll('.hov_img')
+    imgElem.forEach(elem => {
+      elem.addEventListener('mouseover', function(e) {
+        cursor.classList.add('hov_')
+      })
+      elem.addEventListener('mouseout', function(e) {
+        cursor.classList.remove('hov_')
       })
     })
   }

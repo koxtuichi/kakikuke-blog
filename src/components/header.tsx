126097bab6980123cbaac6e2dc8f5cda4735ba9c
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-03-15 20:24:21 +0900

    エラー解消

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import ExtLink from './ext-link'
-import useRouter from 'next/router'
+import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [

6ef44539aa994ed9b00a399204885dd315e37f9c
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-06-18 22:32:28 +0900

    40

--- src/pages/photo/index.tsx
+++ src/pages/photo/index.tsx
@@ -76,7 +76,7 @@ function Photo({ urls }) {
                 alt={url.caption}
                 width={url.width}
                 height={height}
-                quality={50}
+                quality={40}
                 priority={true}
               />
               <div style={{ color: '#3D3D3D', padding: 10, }}>{url.caption}</div>
